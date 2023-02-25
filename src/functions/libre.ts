f6dc8e5d256f273bdc1e3df1e2f9e079a16ac58b
Daniel Brooks <bass_rock@me.com>
2023-02-25 10:47:17 -0800

    converting to typescript and some fixes

---
+++ src/functions/libre.ts
@@ -0,0 +1,161 @@
+import * as crypto from 'crypto';
+import axios from 'axios';
+import dayjs, { Dayjs } from 'dayjs';
+import * as fs from 'fs';
+
+const randomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// const selectData = function (entries) {
+//   // Group dates by day
+//   const groups = entries.reduce((acc, singleEntry) => {
+//     const day = dayjs(singleEntry.timestamp).date();
+
+//     if (!acc[day]) {
+//       acc[day] = [];
+//     }
+
+//     acc[day].push(singleEntry);
+
+//     return acc;
+//   }, {});
+
+//   const result = [];
+
+//   for (const day of Object.values(groups)) {
+//     const dayEntries = day.filter((singleEntry) => {
+//       const hour = dayjs(singleEntry.timestamp).hour();
+
+//       return hour >= 7 && hour < 21;
+//     });
+
+//     const selectionSize = randomInt(8, 10);
+
+//     if (dayEntries.length < selectionSize) {
+//       result.push(...dayEntries);
+//     } else {
+//       // Select 8 dates evenly distributed over the 7am-9pm range
+//       const slots = Array.from({ length: selectionSize }, (_, i) => i);
+//       const slotSize = Math.floor(dayEntries.length / selectionSize);
+//       const slotPositions = slots.map((slot) => slot * slotSize);
+//       for (const pos of slotPositions) {
+//         const slotDates = dayEntries.slice(pos, pos + slotSize);
+//         result.push(slotDates[Math.floor(Math.random() * slotDates.length)]);
+//       }
+//     }
+//   }
+
+//   return result;
+// };
+
+export const authLibreView = async function (
+  username,
+  password,
+  device,
+  setDevice
+) {
+  console.log('authLibreView');
+
+  const data = {
+    DeviceId: device,
+    GatewayType: 'FSLibreLink.iOS',
+    SetDevice: setDevice,
+    UserName: username,
+    Domain: 'Libreview',
+    Password: password,
+  };
+
+  console.log(data);
+
+  const response = await axios.post(
+    'https:/api-us.libreview.io/lsl/api/nisperson/getauthentication',
+    data,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  console.log('authLibreView, response', response.data);
+
+  if (response.data.status !== 0) {
+    return;
+  }
+
+  return response.data.result.UserToken;
+};
+
+export const transferLibreView = async function (
+  device,
+  token,
+  glucoseEntries,
+  foodEntries,
+  insulinEntries
+) {
+  console.log('transferLibreView');
+
+  console.log('glucose entries', (glucoseEntries || []).length.toString());
+  console.log('food entries', (foodEntries || []).length.toString());
+  console.log('insulin entries', (insulinEntries || []).length.toString());
+
+  //const glucoseSelection = selectData(glucoseEntries);
+
+  const data = {
+    UserToken: token,
+    GatewayType: 'FSLibreLink.iOS',
+    DeviceData: {
+      header: {
+        device: {
+          hardwareDescriptor: 'iPhone14,2',
+          osVersion: '15.4.1',
+          modelName: 'com.abbott.librelink.us',
+          osType: 'iOS',
+          uniqueIdentifier: device,
+          hardwareName: 'iPhone',
+        },
+      },
+      measurementLog: {
+        capabilities: [
+          'scheduledContinuousGlucose',
+          'unscheduledContinuousGlucose',
+          'bloodGlucose',
+          'insulin',
+          'food',
+          'generic-com.abbottdiabetescare.informatics.exercise',
+          'generic-com.abbottdiabetescare.informatics.customnote',
+          'generic-com.abbottdiabetescare.informatics.ondemandalarm.low',
+          'generic-com.abbottdiabetescare.informatics.ondemandalarm.high',
+          'generic-com.abbottdiabetescare.informatics.ondemandalarm.projectedlow',
+          'generic-com.abbottdiabetescare.informatics.ondemandalarm.projectedhigh',
+          'generic-com.abbottdiabetescare.informatics.sensorstart',
+          'generic-com.abbottdiabetescare.informatics.error',
+          'generic-com.abbottdiabetescare.informatics.isfGlucoseAlarm',
+          'generic-com.abbottdiabetescare.informatics.alarmSetting',
+        ],
+        bloodGlucoseEntries: [],
+        genericEntries: [],
+        scheduledContinuousGlucoseEntries: glucoseEntries || [],
+        insulinEntries: insulinEntries || [],
+        foodEntries: foodEntries || [],
+        unscheduledContinuousGlucoseEntries: [] || [],
+      },
+    },
+    Domain: 'Libreview',
+  };
+
+  console.log(JSON.stringify(data));
+
+  const response = await axios.post(
+    'https://api-us.libreview.io/lsl/api/measurements',
+    data,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  console.log('transferLibreView, response', response.data);
+};
---
+++ src/main.ts
@@ -0,0 +1,80 @@
+import * as uuid from 'uuid';
+import * as colors from 'colors';
+import * as prompt from 'prompt';
+import * as fs from 'fs';
+import dayjs, { Dayjs } from 'dayjs';
+require('dotenv').config({ path: __dirname + '/../config.env' });
+
+import { authLibreView, transferLibreView } from './functions/libre';
+import {
+  getNightscoutFoodEntries,
+  getNightscoutGlucoseEntries,
+  getNightscoutBasalInsulinEntries,
+  getNightscoutBolusInsulinEntries,
+} from './functions/nightscout';
+
+const CONFIG_NAME = 'config.json';
+const DEFAULT_CONFIG = {};
+
+if (!fs.existsSync(CONFIG_NAME)) {
+  fs.writeFileSync(CONFIG_NAME, JSON.stringify(DEFAULT_CONFIG));
+}
+
+const rawConfig = fs.readFileSync(CONFIG_NAME, 'utf8');
+let config = JSON.parse(rawConfig);
+
+console.log(config);
+
+fs.writeFileSync(CONFIG_NAME, JSON.stringify(config));
+
+(async () => {
+  const fromDate = dayjs(`2022-01-01`).format('YYYY-MM-DD');
+
+  const toDate = dayjs(`2023-02-26`).format('YYYY-MM-DD');
+
+  console.log('transfer time span', fromDate, toDate);
+
+  const glucoseEntries = []; // await getNightscoutGlucoseEntries(config.nightscoutUrl, config.nightscoutToken, fromDate, toDate);
+  const foodEntries = []; //await getNightscoutFoodEntries(config.nightscoutUrl, config.nightscoutToken,fromDate,toDate);
+  const insulinBolusEntries = await getNightscoutBolusInsulinEntries(
+    config.nightscoutUrl,
+    config.nightscoutToken,
+    fromDate,
+    toDate
+  );
+
+  const insulinBasalEntries = await getNightscoutBasalInsulinEntries(
+    config.nightscoutUrl,
+    config.nightscoutToken,
+    fromDate,
+    toDate
+  );
+
+  const insulinEntries = [...insulinBolusEntries, ...insulinBasalEntries];
+
+  if (
+    glucoseEntries.length > 0 ||
+    foodEntries.length > 0 ||
+    insulinEntries.length > 0
+  ) {
+    const auth = await authLibreView(
+      config.libreUsername,
+      config.librePassword,
+      config.libreDevice,
+      true
+    );
+    if (!!!auth) {
+      console.log('libre auth failed!');
+
+      return;
+    }
+
+    await transferLibreView(
+      config.libreDevice,
+      auth,
+      glucoseEntries,
+      foodEntries,
+      insulinEntries
+    );
+  }
+})();
